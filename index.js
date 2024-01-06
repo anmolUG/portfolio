b7cbe333be7222e9bd9105e587be429846dd19ca
anmolUG <155796129+anmolUG@users.noreply.github.com>
2024-01-06 10:22:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var t_ = Object.create;
+    var sn = Object.defineProperty;
+    var r_ = Object.getOwnPropertyDescriptor;
+    var n_ = Object.getOwnPropertyNames;
+    var i_ = Object.getPrototypeOf
+      , o_ = Object.prototype.hasOwnProperty;
+
+// loader 
+
+{
+    "actionItems"; [{
+        "id": "a-9-n-7",
+        "actionTypeId": "STYLE_OPACITY",
+        "config": {
+            "delay": 0,
+            "easing": "ease",
+            "duration": 300,
+            "target": {
+                "selector": ".loader_content-wrapper",
+                "selectorGuids": ["7172727c-3d5a-4a4f-51e8-8995f570b39e"]
+            },
+            "value": 0,
+            "unit": ""
+        }
+    }]
+;
+}
+
+// background video
+"a-25"; {
+    "id"; "a-25",
+    "title"; "darken-section [scroll]",
+    "continuousParameterGroups"; [{
+        "id": "a-25-p",
+        "type": "SCROLL_PROGRESS",
+        "parameterLabel": "Scroll",
+        "continuousActionGroups": {
+            "keyframe": 65,
+            "actionItems": [{
+                "id": "a-25-n",
+                "actionTypeId": "STYLE_BACKGROUND_COLOR",
+                "config": {
+                    "delay": 0,
+                    "easing": "",
+                    "duration": 500,
+                    "target": {
+                        "useEventTarget": "CHILDREN",
+                        "selector": ".video-overlay-layer",
+                        "selectorGuids": ["30d8a5b2-ce8c-36ff-5584-e4a12f172f68"]
+                    },
+                    "globalSwatchId": "",
+                    "rValue": 0,
+                    "bValue": 0,
+                    "gValue": 0,
+                    "aValue": 0.4
+                }
+            }]
+        }
+    }]
+}
